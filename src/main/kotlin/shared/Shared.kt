9c92cf481d3aad79fe726210004723e20dfb7740
Sinisha <skuhtic@gmail.com>
2021-12-17 19:26:20 +0100

    Shared

--- src/main/kotlin/shared/Shared.kt
+++ src/main/kotlin/shared/Shared.kt
@@ -1,25 +1,14 @@
 package shared
 
 data class Position(val x: Int, val y: Int) {
-
-    companion object {
-        var minX: Int = 0
-        var maxX: Int = 0
-        var minY: Int = 0
-        var maxY: Int = 0
+    private val toLeft get() = Position(x - 1, y)
+    private val toUp get() = Position(x, y - 1)
+    private val toRight get() = Position(x + 1, y)
+    private val toDown get() = Position(x, y + 1)
+    private fun crossNeighbours(width: Int, height: Int) = listOf(toLeft, toRight, toUp, toDown).filter {
+        it.x in 0 until width && it.y in 0 until height
     }
 
-    init {
-        maxX = maxOf(maxX, x)
-        minX = maxOf(minX, x)
-        maxY = maxOf(maxY, y)
-        minY = maxOf(minY, y)
-    }
-
-    val toLeft = if (x > 0) Position(x-1, y) else null
-    val toUp = if (y > 0) Position(x, y-1) else null
-    val toRight = if (x < maxX) Position(x+1, y) else null
-    val toDown = if (y < maxY) Position(x, y+1) else null
-
-    val crossNeighbours = listOfNotNull(toLeft, toRight, toUp, toDown)
+    fun crossNeighbours(squareSize: Int) = crossNeighbours(squareSize, squareSize)
 }
+
